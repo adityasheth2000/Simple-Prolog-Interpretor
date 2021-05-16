2fd142218779504f39ce39b27e33aff4db07524d
Aditya Sheth <aditya.sheth@epifi.com>
2021-05-16 16:05:00 +0530

    compilation error resolved, .hpp file updated

--- Interpretor.cpp
+++ Interpretor.cpp
@@ -2,121 +2,105 @@
 // #include<bits/stdc++.h>
 // using namespace std;
 
-struct Variable
-{
-    string var_name;
-    Variable()
-    {
-        mt19937 rng(chrono::steady_clock::now().time_since_epoch().count());
-        int r=rng();
-        var_name="Unknown_"+to_string(r);
-    }
-    Variable(string tmp) : var_name(tmp){}
-    void print()
-    {
-        cout<<var_name;
-    }
-    bool operator<(const Variable &other) const
-    {
-        return var_name<other.var_name;
-    }
-    bool operator==(const Variable &other) const
-    {
-        return var_name==other.var_name;
-    }
-};
-struct Constant
-{
-    bool is_int;
-    string const_str;
-    int const_int;
-    Constant(string tmp) : const_str(tmp), const_int(0),is_int(false) {}
-    Constant(int tmp) : const_int(tmp), const_str(""),is_int(true) {}
-    void print()
-    {
-        if(is_int)
-            cout<<const_int;
-        else
-            cout<<const_str;
-    }
-};
-struct Complex
-{
-    string functor;
-    vector<Term *> subterms;
-    Complex(string functor_name, vector<Term *> subterms_list) : functor(functor_name), subterms(subterms_list) {}
-    void print()
-    {
-        cout<<functor<<"(";
-        for(auto x: subterms)
-        {
-            x->print();
-            cout<<",";
-        }
-        cout<<")";
-    }
-};
-struct Term
-{
-    Variable *variable;
-    Constant *constant;
-    Complex *complex;
-    Term() : variable(NULL), constant(NULL), complex(NULL) {}
-    Term(Variable *tmp) : variable(tmp), constant(NULL), complex(NULL) {}
-    Term(Constant *tmp) : variable(NULL), constant(tmp), complex(NULL) {}
-    Term(Complex *tmp) : variable(NULL), constant(NULL), complex(tmp) {}
-    void print()
-    {
-        if(variable)
-        {
-            variable->print();
-        }
-        else if(constant)
-        {
-            constant->print();
-        }
-        else if(complex)
-        {
-            complex->print();
-        }
-    }
-};
 
-struct DSU_Variables
+Variable::Variable()
 {
-    map<Variable,Variable> parent_variable;
-    map<Variable,int> dsu;
-    bool contains_variable(Variable v){
-        return (dsu.count(v)>0);
-    }
-    void insert_variable(Variable v){
-        parent_variable[v]=Variable("Temporary_variable");
-        dsu[v]=1;
-    }
-    Variable find_parent(Variable a){
-        Variable v=a;
-        while(dsu[v]==0)
-            v=parent_variable[v];
-        return v;
-    }
-    void merge_variables(Variable a, Variable b)
+    mt19937 rng(chrono::steady_clock::now().time_since_epoch().count());
+    int r=rng();
+    var_name="Unknown_"+to_string(r);
+}
+Variable::Variable(string tmp) : var_name(tmp){}
+void Variable::print()
+{
+    cout<<var_name;
+}
+bool Variable::operator<(const Variable &other) const
+{
+    return var_name<other.var_name;
+}
+bool Variable::operator==(const Variable &other) const
+{
+    return var_name==other.var_name;
+}
+
+
+Constant::Constant(string tmp) : const_str(tmp), const_int(0),is_int(false) {}
+Constant::Constant(int tmp) : const_int(tmp), const_str(""),is_int(true) {}
+void Constant::print()
+{
+    if(is_int)
+        cout<<const_int;
+    else
+        cout<<const_str;
+}
+
+
+Complex::Complex(string functor_name, vector<Term *> subterms_list) : functor(functor_name), subterms(subterms_list) {}
+void Complex::print()
+{
+    cout<<functor<<"(";
+    for(auto x: subterms)
     {
-        a=find_parent(a);
-        b=find_parent(b);
-        int sz_a=dsu[a];
-        int sz_b=dsu[b];
-        if(sz_a<=sz_b){
-            parent_variable[a]=b;
-            dsu[b]+=sz_a;
-            dsu[a]=0;
-        }
-        else{
-            parent_variable[a]=a;
-            dsu[a]+=sz_b;
-            dsu[b]=0;
-        }
-    }    
-};
+        x->print();
+        cout<<",";
+    }
+    cout<<")";
+}
+
+
+Term::Term() : variable(NULL), constant(NULL), complex(NULL) {}
+Term::Term(Variable *tmp) : variable(tmp), constant(NULL), complex(NULL) {}
+Term::Term(Constant *tmp) : variable(NULL), constant(tmp), complex(NULL) {}
+Term::Term(Complex *tmp) : variable(NULL), constant(NULL), complex(tmp) {}
+void Term::print()
+{
+    if(variable)
+    {
+        variable->print();
+    }
+    else if(constant)
+    {
+        constant->print();
+    }
+    else if(complex)
+    {
+        complex->print();
+    }
+}
+
+
+
+bool DSU_Variables::contains_variable(Variable v){
+    return (dsu.count(v)>0);
+}
+void DSU_Variables::insert_variable(Variable v){
+    parent_variable[v]=Variable("Temporary_variable");
+    dsu[v]=1;
+}
+Variable DSU_Variables::find_parent(Variable a){
+    Variable v=a;
+    while(dsu[v]==0)
+        v=parent_variable[v];
+    return v;
+}
+void DSU_Variables::merge_variables(Variable a, Variable b)
+{
+    a=find_parent(a);
+    b=find_parent(b);
+    int sz_a=dsu[a];
+    int sz_b=dsu[b];
+    if(sz_a<=sz_b){
+        parent_variable[a]=b;
+        dsu[b]+=sz_a;
+        dsu[a]=0;
+    }
+    else{
+        parent_variable[a]=a;
+        dsu[a]+=sz_b;
+        dsu[b]=0;
+    }
+}    
+
 /*-----------------------Unification part------------------------------------------*/
 
 bool unification(Term *lhs, Term *rhs, map<Variable, Term*> &variable_assignment)
@@ -197,61 +181,52 @@ If yes, add to our answer list else don't.
 */
 
 
-struct KnowledgeBase
-{
-    struct Fact
-    {
-        //Fact can be a term, but term can't be a fact.
-        //Example : Loves(X,Y) := Loves(Y,X) is a fact but not a term.
-        Term* term;
-        vector<Fact> dependencies; // term :- dependencies[0], dependencies[1], ... , dependencies.back().
-    };
 
-    vector<Fact> kb;
-    void rec(int index,vector<vector<map<Variable,Term*>>> &dependencies, map<Variable,Term*> variable_assignment,vector<map<Variable,Term*>> &valid_assignments)
+    
+void KnowledgeBase::rec(int index,vector<vector<map<Variable,Term*>>> &dependencies, map<Variable,Term*> variable_assignment,vector<map<Variable,Term*>> &valid_assignments)
+{
+    if(index==dependencies.size()){
+        valid_assignments.push_back(variable_assignment);
+        return;
+    }
+    for(int i=0;i<dependencies[index].size();i++)
     {
-        if(index==dependencies.size()){
-            valid_assignments.push_back(variable_assignment);
-            return;
-        }
-        for(int i=0;i<dependencies[index].size();i++)
+        auto vas=dependencies[index][i];
+        bool flag=1;
+        auto cpy_variable_assignment=variable_assignment;
+        for(auto x: vas)
         {
-            auto vas=dependencies[index][i];
-            bool flag=1;
-            auto cpy_variable_assignment=variable_assignment;
-            for(auto x: vas)
-            {
-                if(cpy_variable_assignment.count(x.first)){
-                    flag=flag&unification(x.second,cpy_variable_assignment[x.first],cpy_variable_assignment);
-                }
-                if(!flag)
-                    break;
+            if(cpy_variable_assignment.count(x.first)){
+                flag=flag&unification(x.second,cpy_variable_assignment[x.first],cpy_variable_assignment);
             }
-            if(flag)
-            {
-                rec(index+1,dependencies,cpy_variable_assignment,valid_assignments);
-            }
+            if(!flag)
+                break;
+        }
+        if(flag)
+        {
+            rec(index+1,dependencies,cpy_variable_assignment,valid_assignments);
         }
     }
-    vector<map<Variable,Term*>> Proof_Search(Fact f)
+}
+vector<map<Variable,Term*>> KnowledgeBase::Proof_Search(Fact f)
+{
+    vector<map<Variable,Term*>> ans;
+    for(auto v: kb)
     {
-        vector<map<Variable,Term*>> ans;
-        for(auto v: kb)
+        map<Variable,Term*> variable_assignment;
+        if(unification(f.term, v.term, variable_assignment))
         {
-            map<Variable,Term*> variable_assignment;
-            if(unification(f.term, v.term, variable_assignment))
-            {
-                vector<vector<map<Variable,Term*>>> Dependencies_Assignment;//index 0 will hold proof search result of 1st dependency and so on.
-                for(int i=0;i<v.dependencies.size();i++){
-                    auto proof_search_result=Proof_Search(v.dependencies[i]);
-                    Dependencies_Assignment.push_back(proof_search_result);
-                }
-                rec(0,Dependencies_Assignment,variable_assignment,ans);
+            vector<vector<map<Variable,Term*>>> Dependencies_Assignment;//index 0 will hold proof search result of 1st dependency and so on.
+            for(int i=0;i<v.dependencies.size();i++){
+                auto proof_search_result=Proof_Search(v.dependencies[i]);
+                Dependencies_Assignment.push_back(proof_search_result);
             }
-        }    
-        return ans;
-    }
-};
+            rec(0,Dependencies_Assignment,variable_assignment,ans);
+        }
+    }    
+    return ans;
+}
+
 
 
 int main()
--- Interpretor.hpp
+++ Interpretor.hpp
@@ -1,15 +1,70 @@
-#include <bits/stdc++.h>
+#include<bits/stdc++.h>
 using namespace std;
 
+struct Variable
+{
+    string var_name;
+    Variable();
+    Variable(string tmp);
+    void print();
+    bool operator<(const Variable &other) const;
+    bool operator==(const Variable &other) const;
+};
+struct Constant
+{
+    bool is_int;
+    string const_str;
+    int const_int;
+    Constant(string tmp); 
+    Constant(int tmp);
+    void print();
+};
 struct Term;
-struct Variable;
-struct Constant;
-struct Complex;
+struct Complex
+{
+    string functor;
+    vector<Term *> subterms;
+    Complex(string functor_name, vector<Term *> subterms_list);
+    void print();
+};
+struct Term
+{
+    Variable *variable;
+    Constant *constant;
+    Complex *complex;
+    Term();
+    Term(Variable *tmp);
+    Term(Constant *tmp);
+    Term(Complex *tmp);
+    void print();
+};
 
-struct KnowledgeBase;
-struct DSU_Variables;
+struct DSU_Variables
+{
+    map<Variable,Variable> parent_variable;
+    map<Variable,int> dsu;
+    bool contains_variable(Variable v);
+    void insert_variable(Variable v);
+    Variable find_parent(Variable a);
+    void merge_variables(Variable a, Variable b);
+};
+/*-----------------------Unification part------------------------------------------*/
 
-bool unification(Term *lhs, Term *rhs, map<Variable, Term *> &variable_assignment);
-bool unify(Term *lhs, Term *rhs, map<Variable, Term *> &variable_assignment, DSU_Variables &dsu);
-bool unify_complex(Complex *lhs, Complex *rhs, map<Variable, Term *> &variable_assignment, DSU_Variables &dsu);
+bool unification(Term *lhs, Term *rhs, map<Variable, Term*> &variable_assignment);
+bool unify(Term *lhs, Term *rhs, map<Variable , Term *> &variable_assignment, DSU_Variables &dsu);
 bool check_constant(Constant *lhs, Constant *rhs);
+bool unify_complex(Complex *lhs, Complex *rhs, map<Variable , Term *> &variable_assignment, DSU_Variables &dsu);
+
+struct KnowledgeBase
+{
+    struct Fact
+    {
+        Term* term;
+        vector<Fact> dependencies; // term :- dependencies[0], dependencies[1], ... , dependencies.back().
+    };
+
+    vector<Fact> kb;
+    void rec(int index,vector<vector<map<Variable,Term*>>> &dependencies, map<Variable,Term*> variable_assignment,vector<map<Variable,Term*>> &valid_assignments);
+    vector<map<Variable,Term*>> Proof_Search(Fact f);
+};
+
