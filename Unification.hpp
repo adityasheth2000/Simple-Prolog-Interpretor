9aebfc3c085e01daf7d4897f53413a7ab2186383
Aditya Sheth <aditya.sheth@epifi.com>
2021-05-18 21:12:11 +0530

    Files splitted into proper header files and .cpp files

---
+++ Unification.hpp
@@ -0,0 +1,9 @@
+#ifndef STRUCTURES
+#define STRUCTURES
+#include "Structures.hpp"
+#endif
+
+bool unification(Term *lhs, Term *rhs, map<Variable, Term*> &variable_assignment);
+bool unify(Term *lhs, Term *rhs, map<Variable , Term *> &variable_assignment, DSU_Variables &dsu);
+bool check_constant(Constant *lhs, Constant *rhs);
+bool unify_complex(Complex *lhs, Complex *rhs, map<Variable , Term *> &variable_assignment, DSU_Variables &dsu);
---
+++ parser.hpp
@@ -0,0 +1,10 @@
+#ifndef STRUCTURES
+#define STRUCTURES
+#include "Structures.hpp"
+#endif
+
+
+Term* make_Term(string s);
+Complex* make_Complex(string s);
+Constant* make_Constant(string s);
+Variable* make_Variable(string s);
