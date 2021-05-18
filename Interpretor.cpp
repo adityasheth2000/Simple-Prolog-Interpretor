80f73fb9ffa96a3c25bb273a9875f401e5c678e0
Aditya Sheth <aditya.sheth@epifi.com>
2021-05-18 20:37:32 +0530

    code-almost-ready

--- Interpretor.cpp
+++ Interpretor.cpp
@@ -359,14 +359,68 @@ Variable* make_Variable(string s)
     return v;
 }
 
+void proof_search_result_output(vector<map<Variable,Term*>> ans)
+{
+    if(ans.size()==0)   
+    {
+        cout<<"no results!";
+        return;
+    }
+    for(auto vas: ans)
+    {
+        cout<<"{\n";
+        for(auto x: vas)
+        {
+            cout<<"\t";
+            Variable v=x.first;
+            v.print();
+            cout<<" : ";
+            x.second->print();
+            cout<<endl;
+        }
+        cout<<"}\n";
+    }  
+}
+
 int main()
 { 
     
-    map<Variable, Term *> variable_assignment;
-    Term* lhs=make_Term("h(X,Y,g(X,Y))");
-    Term* rhs=make_Term("h(a,a,g(Y,X))");
-    bool f = unification(lhs,rhs, variable_assignment);
-    tr(f);
-    if (f)
-        print_variable_assignment(variable_assignment);
+    // {
+    //     map<Variable, Term *> variable_assignment;
+    //     Term* lhs=make_Term("");
+    //     Term* rhs=make_Term("");
+    //     lhs->print();
+    //     cout<<endl;
+    //     rhs->print();
+    //     cout<<endl;
+    //     bool f = unification(lhs,rhs, variable_assignment);
+    //     tr(f);
+    //     if (f)
+    //         print_variable_assignment(variable_assignment);
+    // }
+
+    {
+            // happy(yolanda).
+            // listens2Music(mia).
+            // listens2Music(yolanda):-  happy(yolanda).
+            // playsAirGuitar(mia):-  listens2Music(mia).
+            // playsAirGuitar(yolanda):-  listens2Music(yolanda).
+            
+            Fact f1=Fact(make_Term("happy(yolanda)"),{});
+            Fact f2=Fact(make_Term("listens2Music(mia)"),{});
+            Fact f3=Fact(make_Term("listens2Music(yolanda)"),{Fact(make_Term("happy(yolanda)"),{})});
+            Fact f4=Fact(make_Term("playsAirGuitar(mia)"),{Fact(make_Term("listens2Music(mia)"),{})});
+            Fact f5=Fact(make_Term("playsAirGuitar(yolanda)"),{Fact(make_Term("listens2Music(yolanda)"),{})});
+            
+            KnowledgeBase kb;
+            kb.insert_Fact(f1);
+            kb.insert_Fact(f2);
+            kb.insert_Fact(f3);
+            kb.insert_Fact(f4);
+            kb.insert_Fact(f5);
+            
+            Fact qry=Fact(make_Term("listens2Music(X)"),{});
+            auto ans=kb.Proof_Search(qry);
+            proof_search_result_output(ans);
+    }
 }
